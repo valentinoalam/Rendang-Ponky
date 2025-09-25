896a7c94c513ff3085ede9061c38e2d1637e3bf2
Ichikyube <ichikyube@gmail.com>
2025-09-25 09:40:41 +0700

    nana

--- src/components/sections/Faq.tsx
+++ src/components/sections/Faq.tsx
@@ -57,7 +57,7 @@ const faqData = [
 
 export function FaqSection() {
   return (
-    <section className="w-full py-12 md:py-24 lg:py-32 bg-background">
+    <section className="w-full py-12 md:py-24 lg:py-32 bg-background/80 rounded-4xl backdrop-blur-2xl">
       <div className="container px-4 md:px-6">
         <div className="flex flex-col items-center justify-center space-y-4 text-center">
           <div className="space-y-2">
